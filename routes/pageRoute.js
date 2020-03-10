a0e5ac8cfaa35566e3eaa374e8f44658c1383771
Alhaw <1996048@cegep-heritage.qc.ca>
2020-03-10 12:08:16 -0400

    add login

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,19 +1,29 @@
 const router = require("express").Router();
 const pageModel = require("../model/pageModel");
 
-router.get("/", (req, res) => {
-  res.send("<h1>home</h1>");
+router.get("/", async (req, res) => {
+  getPageWithDeafualt(req, res);
 });
 
 router.get("/:key", async (req, res) => {
+  getPageWithDeafualt(req, res);
+});
+
+async function getPageWithDeafualt(req, res) {
+  if (req.params.key === undefined) {
+    req.params.key = "home";
+  }
   let page = await pageModel.getPage(req.params.key);
   let menu = await pageModel.getMenu();
   //console.log(page[0]);
   if (page[0] !== undefined) {
     res.render("pageView", { page: page[0], menu: menu });
   } else {
-    res.send(`<h1>${req.params.key} does not exist</h1>`);
+    req.params.key = "home";
+    getPageWithDeafualt(req, res);
   }
-});
+}
+
+// router.post();
 
 module.exports = router;
