c35fbe2fedf4fdb291b7e93b625838ce65a67a63
Alhaw <1996048@cegep-heritage.qc.ca>
2020-03-10 11:02:59 -0400

    added partial and style

---
+++ model/pageModel.js
@@ -0,0 +1,23 @@
+const db = require("../config/db");
+
+module.exports = {
+  getPage: async function(key) {
+    //key = tolowercase(trim(key));
+    let conn = await db.getConnection();
+    const row = await conn.query(
+      "SELECT pageKey, title,content FROM pages WHERE pageKey = ?",
+      [key]
+    );
+    conn.end();
+    return row;
+  },
+
+  getMenu: async function() {
+    let conn = await db.getConnection();
+    const rows = await conn.query(
+      "SELECT pageKey, title FROM pages WHERE shownInMenu = 1 ORDER BY menuOrder"
+    );
+    conn.end();
+    return rows;
+  }
+};
